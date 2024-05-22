86eac41fa2a0e3e1516305872af6a5c5d0d9bc0d
roll1226 <tomo_yasu_4724@icloud.com>
2024-05-23 07:13:57 +0900

    feat: assets alias

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,7 +1,7 @@
 import App from "@app/App.tsx";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import "@app/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
