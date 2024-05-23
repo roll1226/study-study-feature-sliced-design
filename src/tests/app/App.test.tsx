9c927af64fa4eaaef6a8abdbea90fe82937315aa
roll1226 <tomo_yasu_4724@icloud.com>
2024-05-23 20:40:33 +0900

    change: App.tsx Test

--- src/tests/app/App.test.tsx
+++ src/tests/app/App.test.tsx
@@ -2,10 +2,10 @@ import App from "@app/App";
 import { render, screen } from "@testing-library/react";
 import { describe, expect, it } from "vitest";
 
-describe("App", () => {
-  it("sample test", () => {
+describe("Test App.tsx", () => {
+  it("check show App", () => {
     render(<App />);
-    const linkElement = screen.getByText(/count is/i);
+    const linkElement = screen.getByText(/This is Main Page/i);
     expect(linkElement).toBeInTheDocument();
   });
 });
