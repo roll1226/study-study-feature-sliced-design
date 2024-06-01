6d7f8af82f9730fda67d9ea766c5fde249ea1239
roll1226 <tomo_yasu_4724@icloud.com>
2024-06-02 07:34:12 +0900

    change: slice transition test

--- src/tests/app/App.test.tsx
+++ src/tests/app/App.test.tsx
@@ -4,27 +4,35 @@ import userEvent from "@testing-library/user-event";
 import { MemoryRouter } from "react-router-dom";
 import { describe, expect, it } from "vitest";
 
+const setup = () => {
+  userEvent.setup();
+  render(
+    <MemoryRouter initialEntries={["/"]}>
+      <App />
+    </MemoryRouter>
+  );
+};
+
 describe("Test App.tsx First Main Page", () => {
   it("check show App", () => {
-    render(
-      <MemoryRouter initialEntries={["/"]}>
-        <App />
-      </MemoryRouter>
-    );
+    setup();
     const element = screen.getByText(/This is Main Page/i);
     expect(element).toBeInTheDocument();
   });
 
-  it("Transition App Page", async () => {
-    userEvent.setup();
-    render(
-      <MemoryRouter initialEntries={["/"]}>
-        <App />
-      </MemoryRouter>
-    );
+  it("App Page to Sub Page", async () => {
+    setup();
+    const subPageLink = screen.getByTestId("sub-page-link");
+
+    await waitFor(() => userEvent.click(subPageLink));
+    const subPageElement = screen.getByText(/This is Sub Page/i);
+    expect(subPageElement).toBeInTheDocument();
+  });
+
+  it("App Page to Sub Page to Main Page", async () => {
+    setup();
     const mainPageLink = screen.getByTestId("main-page-link");
     const subPageLink = screen.getByTestId("sub-page-link");
-    const loginPageLink = screen.getByTestId("login-page-link");
 
     await waitFor(() => userEvent.click(subPageLink));
     const subPageElement = screen.getByText(/This is Sub Page/i);
@@ -33,6 +41,11 @@ describe("Test App.tsx First Main Page", () => {
     await waitFor(() => userEvent.click(mainPageLink));
     const mainPageElement = screen.getByText(/This is Main Page/i);
     expect(mainPageElement).toBeInTheDocument();
+  });
+
+  it("App Page to Login Page", async () => {
+    setup();
+    const loginPageLink = screen.getByTestId("login-page-link");
 
     await waitFor(() => userEvent.click(loginPageLink));
     const loginPageElement = screen.getByText(/This is Login Page/i);
