ecf38db726f708b90be9921ced19b4f66d54c334
roll1226 <tomo_yasu_4724@icloud.com>
2024-09-02 07:03:30 +0900

    fix: lazyImport test

--- src/shared/__tests__/utils/lazyImport.test.tsx
+++ src/shared/__tests__/utils/lazyImport.test.tsx
@@ -2,10 +2,8 @@ import { lazyImport } from "@shared/utils/lazyImport";
 import { render, screen } from "@testing-library/react";
 import { Suspense } from "react";
 
-// Mock Component
 const MockComponent = () => <div>Mock Component</div>;
 
-// Mock factory function
 const mockFactory = () => Promise.resolve({ MockComponent });
 
 describe("lazyImport", () => {
@@ -21,13 +19,10 @@ describe("lazyImport", () => {
       </Suspense>
     );
 
-    // The fallback should be rendered while loading
     expect(screen.getByText("Loading...")).toBeInTheDocument();
 
-    // Wait for the lazy component to load
     await screen.findByText("Mock Component");
 
-    // The lazy-loaded component should be rendered
     expect(screen.getByText("Mock Component")).toBeInTheDocument();
   });
 });
